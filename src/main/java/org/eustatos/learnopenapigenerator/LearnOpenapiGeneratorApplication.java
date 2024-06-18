41a0bcc882aae610209e62304667686ece18e87e
Aleksander Astashkin <aleksanderastashkin@MacBook-Pro-Aleksander.local>
2024-06-18 10:45:58 +0400

    initial commit

---
+++ src/main/java/org/eustatos/learnopenapigenerator/LearnOpenapiGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.eustatos.learnopenapigenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOpenapiGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnOpenapiGeneratorApplication.class, args);
+    }
+
+}
